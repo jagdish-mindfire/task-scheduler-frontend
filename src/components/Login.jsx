e92346346c5505bee72ba7d7ce57e1ee6fcd227a
Jagdish <jagdish.p@mindfiresolutions.com>
2024-09-18 13:39:02 +0530

    Add validation in login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,48 +1,90 @@
-import {useState,useEffect,useContext} from 'react'
-import { Link,useNavigate,useLocation } from "react-router-dom"
-import axios from 'axios';
-import { AuthContext } from '../context/AuthContext';
+import { useState, useEffect, useContext } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
+import { AuthContext } from "../context/AuthContext";
 import Modal from "./Modal";
 
 export default function Login() {
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [isValidPassword, setIsValidPassword] = useState(true);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
 
-  const {login} = useContext(AuthContext);
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
 
+  useEffect(() => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (email?.length === 0 || emailRegex.test(email)) {
+      setIsValidEmail(true);
+    } else {
+      setIsValidEmail(false);
+    }
+
+    if (password?.length === 0 || password?.length >= 8) {
+      setIsValidPassword(true);
+    } else {
+      setIsValidPassword(false);
+    }
+
+    if (
+      password?.length === 0 ||
+      !isValidPassword ||
+      email?.length === 0 ||
+      !isValidEmail
+    ) {
+      setButtonDisabled(true);
+    } else {
+      setButtonDisabled(false);
+    }
+    console.log(buttonDisabled);
+  }, [email, password]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const apiUrl = import.meta.env.VITE_API_URL;
-    try {
-      const response = await axios.post(apiUrl+'/auth/login',{email,password});
-      const refreshToken = response.data.refresh_token;
-      const name = response.data?.name;
-      login(refreshToken,name);
-      navigate('/dashboard');
-      
-    } catch (error) {
-      console.log(error.response.data);
-      setErrorMessage(error?.response?.data?.message)
+    if (!buttonDisabled) {
+      const apiUrl = import.meta.env.VITE_API_URL;
+      try {
+        const response = await axios.post(apiUrl + "/auth/login", {
+          email,
+          password,
+        });
+        const refreshToken = response.data.refresh_token;
+        const name = response.data?.name;
+        login(refreshToken, name);
+        navigate("/dashboard");
+      } catch (error) {
+        console.log(error.response.data);
+        setErrorMessage(error?.response?.data?.message);
+      }
     }
   };
 
-  useEffect(()=>{
-    if(location?.state?.showSuccess){
+  useEffect(() => {
+    if (location?.state?.showSuccess) {
       setShowModal(true);
     }
-  },[])
+  }, []);
   return (
     <>
-      <Modal open={showModal} setOpen={setShowModal} title={'Success'} description={'your account created Successfully,Please Login to proceed'} />
+      <Modal
+        open={showModal}
+        setOpen={setShowModal}
+        title={"Success"}
+        description={
+          "your account created Successfully,Please Login to proceed"
+        }
+      />
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <div className="text-center m-6 text-xl font-bold">
-          Welcome To Task Scheduler
-        </div>
+          <div className="text-center m-6 text-xl font-bold">
+            Welcome To Task Scheduler
+          </div>
           <img
             alt="Mindfire"
             src="https://www.mindfiresolutions.com/home-assets/images/logo.webp"
@@ -51,15 +93,16 @@ export default function Login() {
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
-          <h4 className='text-red-500 text-center mt-5'>
-            {errorMessage}
-          </h4>
+          <h4 className="text-red-500 text-center mt-5">{errorMessage}</h4>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form action="#" method="POST" className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Email address
               </label>
               <div className="mt-2">
@@ -69,19 +112,31 @@ export default function Login() {
                   type="email"
                   required
                   value={email}
-                  onChange={(e)=>setEmail(e.target.value)}
+                  onChange={(e) => setEmail(e.target.value)}
                   autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className={
+                    (!isValidEmail
+                      ? " ring-red-700 focus:ring-red-700 "
+                      : "ring-gray-300 focus:ring-indigo-600") +
+                    " block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset  placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6"
+                  }
                 />
               </div>
+              {!isValidEmail && (
+                <label className="text-red-700">
+                  Please Enter a valid email address
+                </label>
+              )}
             </div>
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
                   Password
                 </label>
-               
               </div>
               <div className="mt-2">
                 <input
@@ -90,18 +145,32 @@ export default function Login() {
                   type="password"
                   required
                   value={password}
-                  onChange={(e)=>setPassword(e.target.value)}
+                  onChange={(e) => setPassword(e.target.value)}
                   autoComplete="current-password"
-                  className="block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className={
+                    (!isValidPassword
+                      ? "ring-red-700 focus:ring-red-700"
+                      : "focus:ring-indigo-600 ring-gray-300") +
+                    "block w-full rounded-md border-0 py-1.5 p-2 text-gray-900 shadow-sm ring-1 ring-inset  placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6"
+                  }
                 />
               </div>
+              {!isValidPassword && (
+                <label className="text-red-700">
+                  Password shoud contains minimun 8 chars
+                </label>
+              )}
             </div>
 
             <div>
               <button
-                type="submit"
                 onClick={handleSubmit}
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                disabled={buttonDisabled}
+                className={`flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ${
+                  buttonDisabled
+                    ? "bg-gray-400 cursor-not-allowed"
+                    : "bg-indigo-600 hover:bg-indigo-500 focus-visible:outline-indigo-600"
+                }`}
               >
                 Sign in
               </button>
@@ -109,13 +178,16 @@ export default function Login() {
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Don't have a account ?{' '}
-            <Link to="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-             Sign up
+            Don't have a account ?{" "}
+            <Link
+              to="/signup"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 "
+            >
+              Sign up
             </Link>
           </p>
         </div>
       </div>
     </>
-  )
+  );
 }
