5efdeacae4e74fa1ac94d1decb304aa11837b9bd
Jagdish <jagdish.p@mindfiresolutions.com>
2024-10-01 20:28:45 +0530

    Add assertion on edit task test case

--- src/__tests__/EditTask.test.jsx
+++ src/__tests__/EditTask.test.jsx
@@ -4,7 +4,6 @@ import { TaskContext } from "../context/TaskContext";
 
 // Mock Context
 const mockTaskContext = {
-  edittask: jest.fn(),
   updateTask: jest.fn(),
   taskList: [],
   setTaskList: jest.fn(),
@@ -23,17 +22,20 @@ describe("EditTask Component", () => {
 
   test("Renders successfully", () => {
     render(component);
-    // expect(true).toBeTruthy();
+
   });
 
   test("Updating Form Values", () => {
-    // Setup
 
     const { getByTestId } = render(component);
 
     const title = getByTestId("edittask_title");
     const description = getByTestId("edittask_description");
     const due_date = getByTestId("edittask_due_date");
+
+    expect(title).toBeTruthy();
+    expect(description).toBeTruthy();
+    expect(due_date).toBeTruthy();
 
     fireEvent.change(title, {
       target: { value: "Complete the frontend testing" },
@@ -43,6 +45,10 @@ describe("EditTask Component", () => {
     });
     fireEvent.change(due_date, { target: { value: "2022-01-01 12:00:00" } });
     
+    expect(title.value).toBe("Complete the frontend testing");
+    expect(description.value).toBe("This task needs to be completed.");
+    expect(due_date.value).toBe("2022-01-01T12:00");
+
   });
 
   test("Submitting with values", () => {
@@ -55,6 +61,11 @@ describe("EditTask Component", () => {
     const due_date = getByTestId("edittask_due_date");
     const submitBtn = getByTestId("edittask_submit");
 
+    expect(title).toBeTruthy();
+    expect(description).toBeTruthy();
+    expect(due_date).toBeTruthy();
+    expect(submitBtn).toBeTruthy();
+
     fireEvent.change(title, {
       target: { value: "Complete the frontend testing" },
     });
@@ -62,7 +73,13 @@ describe("EditTask Component", () => {
       target: { value: "This task needs to be completed." },
     });
     fireEvent.change(due_date, { target: { value: "2022-01-01 12:00:00" } });
+    
+    expect(title.value).toBe("Complete the frontend testing");
+    expect(description.value).toBe("This task needs to be completed.");
+    expect(due_date.value).toBe("2022-01-01T12:00");
+    
     fireEvent.click(submitBtn);
+    expect(mockTaskContext.updateTask).toHaveBeenCalled();
 
   });
 
@@ -72,10 +89,10 @@ describe("EditTask Component", () => {
     const { getByTestId } = render(component);
 
     const submitBtn = getByTestId("edittask_submit");
-
+    expect(submitBtn).toBeTruthy();
     
     fireEvent.click(submitBtn);
-
+    // expect(mockTaskContext.updateTask).toHaveBeenCalledTimes(0);
   });
 
 });
